667a734074b5f57cfbe14dd1dd0f16faa1c9c434
root <you@example.com>
2023-06-29 16:19:49 +0000

    classes

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+iexport default class Car {
+  /* eslint-disable */
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // method 
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+}
